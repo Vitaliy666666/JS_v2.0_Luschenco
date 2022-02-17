7092120e7b578eb8648021dc5634616769ff3d7a
Vitaliy666666 <88153421+Vitaliy666666@users.noreply.github.com>
2022-02-17 18:58:48 +0300

    Update unit_03.js

--- unit_03/unit_03.js
+++ unit_03/unit_03.js
@@ -246,6 +246,25 @@ document.querySelector('.b-14').onclick = f14;
 // Дан select .s-151 и .s-152, каждый из которых содержит 1 и 0.  Дан select .s-153, который содержит две операции - && и || . Дана кнопка .b-15, при нажатии на которую срабатывает функция f15. Функция выводит в .out-15 результат логических операций выбранных в 3 select к числам выбранным в первом и втором select. Например выбрано 1 1 &&, нужно вывести результат операции 1&&1 т.е. 1 или 0.
 
 function f15(){
+   let sA = +document.querySelector('.s-151').value;
+   let sB = +document.querySelector('.s-152').value;
+   let selC = document.querySelector('.s-153').value;
+   let out = document.querySelector('.out-15');
+   switch (selC) {
+      case '&&':
+         out.innerHTML = sA && sB;
+      break; 
+      case '||':
+         out.innerHTML = sA || sB;
+      break;      
+      
+
+
+
+   }
+      
+
+   //console.log(selC);
 
 }
 
