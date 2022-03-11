a17db6b409f6b544444a3d714010918da6506ba5
Vitaliy666666 <88153421+Vitaliy666666@users.noreply.github.com>
2022-03-11 22:23:56 +0300

    7/15

--- unit_08/unit_08.js
+++ unit_08/unit_08.js
@@ -131,7 +131,15 @@ document.querySelector('.b-6').onclick = t6;
 // Задача решается с помощью цикла  while.
 
 function t7() {
-
+    let out = document.querySelector('.out-7');
+    let inp = document.querySelector('.i-7').value;
+    let out1 = '';
+    let a = 0;
+    while(inp >= a){
+        out1 += inp + '_';
+        inp--
+    }
+    out.innerHTML = out1;
 }
 
 document.querySelector('.b-7').onclick = t7;
