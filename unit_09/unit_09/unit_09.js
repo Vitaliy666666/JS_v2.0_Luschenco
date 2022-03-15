93dca8aa4642c3f70ea2057fdae72c67317d1872
Vitaliy666666 <88153421+Vitaliy666666@users.noreply.github.com>
2022-03-15 18:09:04 +0300

    1/20

--- unit_09/unit_09/unit_09.js
+++ unit_09/unit_09/unit_09.js
@@ -3,7 +3,9 @@
 
 
 function f1() {
-
+    out1 = document.querySelector('.out-1');
+    out1.style.height = '200px';
+    out1.style.width = '90px';
 }
 
 document.querySelector('.b-1').onclick = f1;
