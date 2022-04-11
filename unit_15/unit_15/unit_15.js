2e4517da592c816c207612983ec19a9b2c0d29dd
Vitaliy666666 <88153421+Vitaliy666666@users.noreply.github.com>
2022-04-11 22:37:26 +0300

    8/13

--- unit_15/unit_15/unit_15.js
+++ unit_15/unit_15/unit_15.js
@@ -106,7 +106,12 @@ document.querySelector('.b-7').onclick = f7;
 let s8 = new Set([1, 2, 3, 4, 5, 3, 4, 7, 9, 5, 7, 8, 9, 23, 45, 5, 2, 4, 5, 3, 24, 5, 2, 4, 56, 4, 3, 2, 335, 2, 23, 41, 3, 4, 1, 1, 4, 2, 2, 4, 5, 24, 5, 3, 22, 56]);
 let ar8 = [];
 
-const f8 = () => { }
+const f8 = () => {
+    for(let i of s8){
+        if(i > 5) ar8.push(i);
+    }
+    console.log(ar8);
+}
 
 document.querySelector('.b-8').onclick = f8;
 
